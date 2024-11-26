aa3bffa9d33c310156cca719f780a23593dd2cca
Metronon <bumpercar07@gmail.com>
2024-11-26 11:43:09 +0900

    plumultiply test 및 구현

--- src/main/java/com/ll/SimpleCalculator.java
+++ src/main/java/com/ll/SimpleCalculator.java
@@ -1,5 +1,11 @@
 package com.ll;
 
 public class SimpleCalculator {
+    public int plus(int num1, int num2) {
+        return num1 + num2;
+    }
 
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
 }
--- src/test/java/com/ll/SimpleCalculatorTest.java
+++ src/test/java/com/ll/SimpleCalculatorTest.java
@@ -1,4 +1,62 @@
 package com.ll;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SimpleCalculatorTest {
+    @Test
+    @DisplayName("1 + 2 = 3")
+    public void t1() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.plus(1, 2);
+
+        assertThat(rs).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("2 + 10 = 12")
+    public void t2() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.plus(2, 10);
+
+        assertThat(rs).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("20 + 10 = 30")
+    public void t3() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.plus(20, 10);
+
+        assertThat(rs).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("2 * 6 = 12")
+    public void t4() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.multiply(2, 6);
+
+        assertThat(rs).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("5 * 4 = 20")
+    public void t5() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.multiply(5, 4);
+
+        assertThat(rs).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("9 * 7 = 63")
+    public void t6() {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        int rs = simpleCalculator.multiply(9, 7);
+
+        assertThat(rs).isEqualTo(63);
+    }
 }
